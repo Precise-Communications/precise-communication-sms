a5336d7ff7103c1d75db984596336d6f1f87a9c0
ishanjirety <ishanjirety24@gmail.com>
2021-01-21 19:52:44 +0530

    _app.js fix 2

--- pages/_app.js
+++ pages/_app.js
@@ -39,12 +39,6 @@ class MyApp extends App {
 }
 
 MyApp.getInitialProps = async ({ ctx }) => {
-  const { shop } = ctx.session;
-        ctx.cookies.set("shopOrigin", shop, {
-          httpOnly: false,
-          secure: true,
-          sameSite: 'none'
-        });
     return {
       shopOrigin: ctx.query.shop,
     }
