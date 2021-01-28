28b23ea3317b0a8836428d40d936d4c63553d94b
ishanjirety <ishanjirety24@gmail.com>
2021-01-28 21:16:53 +0530

    app bridge fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,26 @@
-import React from "react";
+import React from 'react'
 import App from 'next/app';
-
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
+import Cookies from "js-cookie";
 import '@shopify/polaris/dist/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
-import { Provider } from '@shopify/app-bridge-react';
-import ClientRouter from '../components/ClientRouter';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import ClientRouter from '../components/ClientRouter';
 
 const client = new ApolloClient({
   fetchOptions: {
-    credentials: 'include'
+    credentials: 'include',
   },
 });
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps, shopOrigin } = this.props;
+    const { Component, pageProps } = this.props;
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
 
-    const config = { apiKey: API_KEY, shopOrigin:shopOrigin, forceRedirect: true };
     return (
       <React.Fragment>
         <Head>
@@ -28,19 +28,16 @@ class MyApp extends App {
           <meta charSet="utf-8" />
         </Head>
         <Provider config={config}>
-        <ClientRouter />
-        <AppProvider i18n={translations}>
-        <Component {...pageProps} />
-        </AppProvider>
+          <ClientRouter />
+          <AppProvider i18n={translations}>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
+          </AppProvider>
         </Provider>
       </React.Fragment>
     );
   }
 }
 
-MyApp.getInitialProps = async ({ ctx }) => {
-    return {
-      shopOrigin: ctx.query.shop,
-    }
-  }
 export default MyApp;
